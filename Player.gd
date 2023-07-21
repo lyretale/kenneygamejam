7792a52156b29af3ebde6a9d93311df2065064ad
lyredev <dev@lyretale.com>
2023-07-21 16:40:37 -0700

    Adds player and world scene and player stats

---
+++ Player.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+export var player_stats: Resource = null
+
+var max_speed = player_stats.normal_speed
+var direction := Vector2.ZERO
+var desired_velocity := Vector2.ZERO
+var steering_vector := Vector2.ZERO
+var velocity := Vector2.ZERO
+
+
+func _process(delta: float) -> void:
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = max_speed * direction
+
+	if Input.is_action_just_pressed("boost"):
+		get_node("Timer").start()
+		max_speed = player_stats.boost_speed
+
+	desired_velocity = max_speed * direction
+	steering_vector = desired_velocity - velocity
+	velocity += steering_vector * player_stats.drag_factor
+
+	position += velocity * delta
+	if direction:
+		rotation = velocity.angle()
+
+
+func _on_Boost_timeout() -> void:
+	max_speed = player_stats.normal_speed
---
+++ PlayerStats.gd
@@ -0,0 +1,29 @@
+class_name PlayerStats
+extends Resource
+
+export var health := 3 setget set_health
+export var boost_speed := 1500.0 setget set_boost_speed
+export var normal_speed := 600.0 setget set_normal_speed
+export var drag_factor := 0.1 setget set_drag_factor
+
+func save() -> void:
+	ResourceSaver.save(resource_path, self)
+
+func set_health(new_health: int) -> void:
+	health = new_health
+	
+	# Add code to update UI here
+	
+	save()
+
+func set_boost_speed(new_boost_speed: float) -> void:
+	boost_speed = new_boost_speed
+	save()
+
+func set_normal_speed(new_normal_speed: float) -> void:
+	normal_speed = new_normal_speed
+	save()
+
+func set_drag_factor(new_drag_factor: float) -> void:
+	drag_factor = new_drag_factor
+	save()

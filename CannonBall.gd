b5b6dcb05ecd2d5924523d215b6fd0ad08a97190
lyredev <dev@lyretale.com>
2023-07-21 22:14:03 -0700

    Adds cannon to turret

---
+++ CannonBall.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+export var speed := 350.0
+export var damage := 10.0
+
+onready var timer := $Timer
+
+func _ready() -> void:
+	connect("body_entered", self, "_on_body_entered")
+	timer.connect("timeout", self, "explode")
+
+func _physics_process(delta: float) -> void:
+	_move(delta)
+
+func _move(delta: float) -> void:
+	position += transform.x * speed * delta
+
+func explode() -> void:
+	# We load the explosion scene and instantiate it.
+	#var explosion := preload("Explosion.tscn").instance()
+	# Then, we place the explosion where the rocket is.
+	#explosion.global_position = global_position
+	# We append the explosion to the main scene's children.
+	#get_tree().root.add_child(explosion)
+	# And delete the rocket itself.
+	queue_free()
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	explode()
--- Turret.gd
+++ Turret.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 export (float, 0.01, 1.0) var rotation_factor := 0.1
-onready var timer := $Timer
+#onready var timer := $Timer
 onready var cannon := $Sprite/Position2D
 onready var sprite := $Sprite
 
@@ -11,6 +11,7 @@ func _ready() -> void:
 	# timer.connect("timeout", self, "_on_Timer_timeout")
 	connect("mouse_entered", self, "_on_mouse_entered")
 	connect("mouse_exited", self, "_on_mouse_exited")
+	input_pickable = true
 
 func _physics_process(_delta: float) -> void:
 	_rotate_to_mouse()
@@ -21,6 +22,22 @@ func _rotate_to_mouse() -> void:
 		target_angle = get_global_mouse_position().angle_to_point(global_position)
 	sprite.rotation = lerp_angle(sprite.rotation, target_angle, rotation_factor)
 
+func _input_event(viewport: Object, event: InputEvent, shape_idx: int) -> void:
+	if (
+		event is InputEventMouseButton
+		and event.button_index == BUTTON_LEFT
+		and event.is_pressed()
+	):
+		fire()
+
+func fire():
+	if not in_range:
+		return
+	var cannon_ball: Area2D = preload("CannonBall.tscn").instance()
+	add_child(cannon_ball)
+	
+	cannon_ball.global_transform = cannon.global_transform
+	
 
 func _on_mouse_entered() -> void:
 	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)

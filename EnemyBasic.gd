b33a4b03ea8b1366b8a66bfe5c6239602a6620c1
lyredev <dev@lyretale.com>
2023-07-22 02:02:00 -0700

    Testing

--- EnemyBasic.gd
+++ EnemyBasic.gd
@@ -6,13 +6,15 @@ var target: KinematicBody2D
 # The Area2D that detects the player.
 onready var aggro_area := $AggroArea
 
-export var max_speed := 200.0
-export var drag_factor := 0.5
+export var max_speed := 400.0
+export var drag_factor := 0.1
 
 var velocity := Vector2.ZERO
 var desired_velocity := Vector2.ZERO
 var steering_vector := Vector2.ZERO
 var direction := Vector2.ZERO
+var angle = 0
+var radius = 50
 
 
 func _ready() -> void:
@@ -20,9 +22,10 @@ func _ready() -> void:
 	aggro_area.connect("body_exited", self, "_on_player_exited")
 
 func _physics_process(delta: float) -> void:
-	direction = Vector2.RIGHT
+	direction = Vector2(0,0)
 	if target:
-		direction = to_local(target.global_position).normalized()
+		direction = to_local(target.global_position).normalized() #+ Vector2.RIGHT.rotated(angle)*radius
+		#angle += 0.5
 		
 
 	desired_velocity = max_speed * direction
@@ -32,6 +35,7 @@ func _physics_process(delta: float) -> void:
 	position += move_and_slide(velocity * delta)
 	
 	rotation = velocity.angle()
+	
 
 func _on_player_entered(body: KinematicBody2D) -> void:
 	# Sets the target when it enters the Area2D.

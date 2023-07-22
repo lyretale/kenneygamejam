320ad38833d8e1dc1bf7e3ac1d5571f76c196c29
lyreteam <team@lyretale.com>
2023-07-21 23:24:56 -0700

    Enemy and layer mask names

--- EnemyBasic.gd
+++ EnemyBasic.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-
+onready var aggro_area := $AggroArea
+var target: KinematicBody2D
 
 export var max_speed := 250.0
 export var drag_factor := 0.1
@@ -10,15 +11,26 @@ var direction := Vector2.ZERO
 var desired_velocity := Vector2.ZERO
 var steering_vector := Vector2.ZERO
 
+func _ready() -> void:
+	aggro_area.connect("body_entered", self, "_on_player_entered")
+	aggro_area.connect("body_exited", self, "_on_player_exited")
+	
+func _on_player_entered(player: KinematicBody2D) -> void:
+	# Sets the target to the robot when it enters the Area2D.
+	target = player
+
+
+func _on_player_exited(robot: KinematicBody2D) -> void:
+	# When the robot exits the Area2D, the target is out of range.
+	target = null
+
 func _physics_process(delta: float) -> void:
 	# We start by calculating the bat's movement direction.
 	var direction := Vector2.ZERO
-	var relative_cursor_position := get_local_mouse_position()
-	# If the distance to the mouse is less than 500 pixels,
-	if relative_cursor_position.length() < 500:
-		# We convert the mouse's local position into a direction vector by
-		# normalizing the position.
-		direction = relative_cursor_position.normalized()
+	
+	if target:
+		direction = to_local(target.global_position).normalized()
+
 
 	var desired_velocity := max_speed * direction
 	var steering_vector := desired_velocity - velocity

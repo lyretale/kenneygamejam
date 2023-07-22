42e6309729400b627f87920195ad19f4bc5222b0
lyreteam <team@lyretale.com>
2023-07-22 14:35:42 -0700

    Objective graohics

---
+++ Node2D.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _ready() -> void:
+	draw_circle(Vector2(100,-100), 700.0, Color(255, 0, 255))
---
+++ Objective.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+onready var objective_collision := $ObjectiveCollision
+
+func _ready() -> void:
+	pass
+
+
+
+
+
+
